6153bfba587f14d57d3e9aa900af3adb29be9cc0
Luciano Decio Neto <lucianodeciopacheco@gmail.com>
2023-08-30 18:29:19 -0300

    Pokedex - Alimentando Vo Pokemon detalhes zoom

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -16,17 +16,102 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     pokemon.type = type
 
     //Abilidades:
-    pokemon.abilities = pokeDetail.abilities.map((ability) => ability.ability.name)    
+    pokemon.abilities = pokeDetail.abilities.map((ability) => ability.ability.name)
 
     //Espécies (URL) -> Vamos precisar buscar as espécies que são separadas por lingua, vamos considerar apenas a "en"
-    pokemon.urlspecies = pokeDetail.species.url
+    pokemon.urlSpecie = pokeDetail.species.url
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
+    //Carrega Specie (URL):
+    pokeApi.getPokemonSpecies(pokemon);
+
+    //Carrega Base Status
+    chargedBaseStatus(pokemon, pokeDetail.stats);
+    
     //Alimenta nosso Map para buscar informações para o dialog depois:
     //Como o id é único, vamos utiliza-lo como chave do map.
     listPokemon.set(pokeDetail.id, pokemon);
     return pokemon
+}
+
+function chargedBaseStatus(pokemon, stats) {
+    //Base Status:
+    let total = 0;
+    for (let i = 0; i < stats.length; i++) {
+        switch (stats[i].stat.name) {
+            case "hp":
+                pokemon.base_status.hp = stats[i].base_stat;
+                total += stats[i].base_stat;
+                break;
+            case "attack":
+                pokemon.base_status.atack = stats[i].base_stat;
+                total += stats[i].base_stat;
+                break;
+            case "defense":
+                pokemon.base_status.defense = stats[i].base_stat;
+                total += stats[i].base_stat;
+                break;
+            case "special-attack":
+                pokemon.base_status.special_attack = stats[i].base_stat;
+                total += stats[i].base_stat;
+                break;
+            case "special-defense":
+                pokemon.base_status.special_defense = stats[i].base_stat;
+                total += stats[i].base_stat;
+                break;
+            case "speed":
+                pokemon.base_status.speed = stats[i].base_stat;
+                total += stats[i].base_stat;
+                break;
+            default:
+                break;
+        }
+    }
+
+    pokemon.base_status.total = total;
+}
+
+
+function chargedSpecieGeneraGenusLanguageEn(pokemon) {
+    //Specie.Genera.Genus:
+    for (let i = 0; i < pokemon.specie.genera.length; i++) {
+        if (pokemon.specie.genera[i].language.name === 'en') {
+            pokemon.specieGenus = pokemon.specie.genera[i].genus;
+            break;
+        }
+    }
+
+    //Specie.egg_groups:
+    pokemon.egg_groups = pokemon.specie.egg_groups.map((egg) => egg.name)
+}
+
+/**
+ * Executa URL Specie e alimenta no VO - Pokemon.specie
+ * 
+ * @param {} pokemon 
+ */
+pokeApi.getPokemonSpecies = (pokemon) => {
+    // Após retorno da promise ele carrega os dados de espécie no VO (Pokemon)
+    if (pokemon !== null) {
+        if (pokemon !== undefined) {
+            fetch(pokemon.urlSpecie)
+                .then((response) => response.json())
+                .then((pokemonsDetails) => pokemonsDetails)
+                .then((result) => chargedSpeciePokemon(pokemon, result))
+        }
+    }
+}
+
+function chargedSpeciePokemon(pokemon, specie) {
+    if (pokemon !== null) {
+        if (pokemon !== undefined) {
+            pokemon.specie = specie;
+            chargedSpecieGeneraGenusLanguageEn(pokemon);
+        }
+    }
+
+    return pokemon;
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -7,6 +7,19 @@ class Pokemon {
     abilities = [];
     weight;
     height;
-    urlspecies;
+    urlSpecie; //URL Specie
+    base_happiness;
+    specie; // Objeto Especie com todos os recursos dele
+    specieGenus;
+    egg_groups = [];
     photo;
+    base_status = {
+        hp: 0,
+        atack: 0,
+        defense: 0,
+        special_attack: 0,
+        special_defense: 0,
+        speed: 0,
+        total: 0
+    };
 }
